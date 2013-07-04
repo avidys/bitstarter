22085cb2f53ef5fb41993cec7c56d2e1142470c5
avidys <jm@avidys.com>
2013-07-04 12:18:15 -0700

    first change

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
 var express = require('express');
+var home = require('./home.js');
+//var util = require('csv');
+
+function User (username, surname) {
+	this.username = username;
+}
+
+user = new User('test','test1');
+
+var port = process.env.PORT || 5000;
+console.log("Starting Server on port "+port);
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.set('title','coursera');
+app.get('/', home.home);
+app.get('/test', home.users);
 
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+console.log("App started.");
+
