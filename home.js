1be6f5123a7f843db2b59c424500ada2bb2ac995
avidys <jm@avidys.com>
2013-07-04 12:31:03 -0700

    added home

---
+++ home.js
@@ -0,0 +1,33 @@
+exports.home = function (request, response) {
+//  response.send('Hello: '+request.path+'<p>');
+  response.writeHead(200,{"Content-Type": "text/html"});
+  response.write('Welcome: <p>');
+  response.write('<li>'+request.host+'</li>');
+  response.write('<li>'+request.request+'</li>');
+  response.write('<li>'+request.path+'</li>');
+  response.end();
+}
+
+//var pg = require('pg');
+
+exports.users = function (request, response) {
+	  response.writeHead(200,{"Content-Type": "text/html"});
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		  var query = client.query('SELECT * FROM users');
+
+		  query.on('row', function(row) {
+			  response.write('<li>'+row+'</li>');
+ 		      console.log(JSON.stringify(row));
+		  });
+		});
+
+
+//  for ( var i = 0; i < 10; i++) {
+//	  response.write('<li>'+i+'</li>');
+//  }
+  response.end();
+	
+}
+
+
+
