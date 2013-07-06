c070d90958c6f33cda108ea8b064542c2a299242
avidys <jm@avidys.com>
2013-07-05 18:06:58 -0700

    cleanup

--- web.js
+++ web.js
@@ -1,28 +1,18 @@
+// coursera startup - bitstarter
 
 var express = require('express');
-
-var fs=require('fs')
-
-var home = require('./home.js');
-//var util = require('csv');
-
-function User (username, surname) {
-	this.username = username;
-}
-
-user = new User('test','test1');
+var home = require('./controller.js'); 
 
 var port = process.env.PORT || 5000;
 console.log("Starting Server on port "+port);
 
 var app = express.createServer(express.logger());
 
-app.set('title','coursera');
+app.set('title','coursera-bitstarter');
 app.get('/', home.home);
-app.get('/test', home.users);
+app.get('/test', home.info);
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function() { console.log("Listening on " + port); });
+
 console.log("App started.");
 
