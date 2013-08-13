5e04ff0dd37c5dd7117911f5b57fb0a1eaa6cb0b
avidys <jm@avidys.com>
2013-08-13 15:51:13 -0700

    staging+dev

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-bitstarter
-==========
+# git workflow
 
+local 
+- dev 
+- staging + tag s1.0
+
+github
+- dev
+- staging
+- prod + tag v1.0
+
+heroku
+- staging
+- prod
+
+1. dev commit
+2. local test
+3. push github dev (saving/history)
+...
+4. branch -b staging or checkout staging
+4.1. merge dev
+4.2 tag -a Cx.y-r01-2013.06.12
+5. local test, change + commit
+...
+6. push github + heroku
+7. heroku test, local change + commit + push
+...
+8. checkout prod 
+9. merge staging
+10. tag -a/-s Vx.y-2013.06.14 -m
+11. push github prod + heroku, push github --tags
+
+12. local checkout dev
+13. merge staging 
+14. goto 1
+
+ 
+# 
 
--- controller3.js
+++ controller3.js
@@ -1,15 +1,15 @@
 
+var fs=require('fs');
 var less = require('less'); // better pre-compile
-var filename = "index_jquery.html"
+var filename = "index.html"
 
 IDEAS_DEFAULT = "ideas.yaml";	
 
-var 
+var yaml = require('js-yaml'),
     path=require('path'),
 	fs   = require('fs'),
 	util = require('util');
 
-// var yaml = require('js-yaml');
 var loadText= function(fn) {
 //	var s = fs.readFileSync(checksfile);
 //    return JSON.parse(s);
@@ -36,13 +36,30 @@ var loadText= function(fn) {
 
 //Get document, or throw exception on error
 
+
 // assignement 3_1
 exports.home = function (request, response) {
-	var s = fs.readFileSync(filename)
-    response.send(s.toString());
+	var s = fs.readFileSync(filename).toString()
+	var o = loadText(IDEAS_DEFAULT);
+	//console.log(o.xhelp);
+	//for ( var int = 0; int < o.length; int++) {
+		//console.log(o[int].id+": "+o[int].sentence+" = "+o[int].section0);
+	//} 
+	s = s.replace("{title}",o.xhelp.id);
+	s = s.replace("{id}",o.xhelp.id);
+	s = s.replace("{sentence}",o.xhelp.sentence)
+	s = s.replace("{section0]",o.xhelp.section0)
+	s = s.replace("{section1]",o.xhelp.section1)
+	s = s.replace("{section2}",o.xhelp.section2)
+	s = s.replace("{section3}",o.xhelp.section3)
+	s = s.replace("{faq_q1}",o.xhelp.faq_q1)
+	s = s.replace("{faq_a1}",o.xhelp.faq_a1)
+	s = s.replace("{faq_q2}",o.xhelp.faq_q2)
+	s = s.replace("{faq_a2}",o.xhelp.faq_a2)
+	s = s.replace("{faq_q3}",o.xhelp.faq_q3)
+	s = s.replace("{faq_a3}",o.xhelp.faq_a3)
+    response.send(s);
 }
-
-
 
 // some experiment
 exports.info = function (request, response) {
@@ -58,8 +75,9 @@ exports.info = function (request, response) {
 if(require.main == module) {
 	//console.log(loadText(IDEAS_DEFAULT));
 	var o = loadText(IDEAS_DEFAULT);
+	console.log(o.xhelp.id);
 	for ( var int = 0; int < o.length; int++) {
-		console.log(o[int].product+": "+o[int].sentence);
+		console.log(o[int].id+": "+o[int].sentence+" = "+o[int].section0);
 	} 
 	
 }
